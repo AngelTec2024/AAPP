5eb57509bcaeece6792722035c6094f7170c0761
ANGEL EDUARDO LOPEZ VEGA <L21540427@delicias.tecnm.mx>
2024-05-07 22:52:02 -0600

    Agregar archivos de proyecto.

---
+++ PPPP/InterfazEdicion.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace PPPP
+{
+    partial class InterfazEdicion
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnPrevisualizacion = new System.Windows.Forms.Panel();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.NCopias = new System.Windows.Forms.NumericUpDown();
+            this.PanelPre = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Acercar = new System.Windows.Forms.Button();
+            this.Alejar = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NCopias)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnPrevisualizacion
+            // 
+            this.pnPrevisualizacion.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pnPrevisualizacion.Location = new System.Drawing.Point(229, 47);
+            this.pnPrevisualizacion.Name = "pnPrevisualizacion";
+            this.pnPrevisualizacion.Size = new System.Drawing.Size(445, 356);
+            this.pnPrevisualizacion.TabIndex = 0;
+            this.pnPrevisualizacion.Paint += new System.Windows.Forms.PaintEventHandler(this.pnPrevisualizacion_Paint);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(46, 617);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(482, 79);
+            this.btnGuardar.TabIndex = 1;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(46, 518);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(482, 62);
+            this.btnSalir.TabIndex = 2;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(251, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Barra de herramientas";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Location = new System.Drawing.Point(137, 141);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(57, 238);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.button7);
+            this.panel2.Location = new System.Drawing.Point(46, 122);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(85, 257);
+            this.panel2.TabIndex = 5;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(0, 184);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(85, 40);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 40);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Ajustar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 40);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Recortar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(0, 73);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 40);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Ajustar y Recortar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(0, 110);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 40);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(0, 147);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(85, 40);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(0, 219);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(85, 38);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "AtoAjustarImgH";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "\"Archivos de imagen|*.jpg;*.jpeg;*.png;*.gif;*.bmp\"";
+            // 
+            // NCopias
+            // 
+            this.NCopias.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NCopias.Location = new System.Drawing.Point(530, 439);
+            this.NCopias.Name = "NCopias";
+            this.NCopias.Size = new System.Drawing.Size(111, 35);
+            this.NCopias.TabIndex = 8;
+            this.NCopias.ValueChanged += new System.EventHandler(this.NCopias_ValueChanged);
+            // 
+            // PanelPre
+            // 
+            this.PanelPre.AutoScroll = true;
+            this.PanelPre.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.PanelPre.Location = new System.Drawing.Point(720, 47);
+            this.PanelPre.Name = "PanelPre";
+            this.PanelPre.Size = new System.Drawing.Size(724, 647);
+            this.PanelPre.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(238, 424);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 31);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Numero de Copias";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Acercar
+            // 
+            this.Acercar.BackgroundImage = global::PPPP.Properties.Resources.boton_para_acercar__1_;
+            this.Acercar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Acercar.Location = new System.Drawing.Point(1086, 700);
+            this.Acercar.Name = "Acercar";
+            this.Acercar.Size = new System.Drawing.Size(70, 70);
+            this.Acercar.TabIndex = 7;
+            this.Acercar.UseVisualStyleBackColor = true;
+            this.Acercar.Click += new System.EventHandler(this.btnZoomIn_Click);
+            // 
+            // Alejar
+            // 
+            this.Alejar.BackgroundImage = global::PPPP.Properties.Resources.lupa_con_signo_menos__2_;
+            this.Alejar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Alejar.Location = new System.Drawing.Point(857, 700);
+            this.Alejar.Name = "Alejar";
+            this.Alejar.Size = new System.Drawing.Size(70, 70);
+            this.Alejar.TabIndex = 6;
+            this.Alejar.UseVisualStyleBackColor = true;
+            this.Alejar.Click += new System.EventHandler(this.btnZoomOut_Click);
+            // 
+            // InterfazEdicion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1465, 793);
+            this.Controls.Add(this.Acercar);
+            this.Controls.Add(this.Alejar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PanelPre);
+            this.Controls.Add(this.NCopias);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.pnPrevisualizacion);
+            this.Name = "InterfazEdicion";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.NCopias)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnPrevisualizacion;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button Alejar;
+        private System.Windows.Forms.Button Acercar;
+        private System.Windows.Forms.NumericUpDown NCopias;
+        private System.Windows.Forms.Panel PanelPre;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PPPP/InterfazEdicion.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PPPP
+{
+    public partial class InterfazEdicion : Form
+    {
+        public StreamReader lector;
+        PictureBox Hoja;
+        int NC;
+        private List<Image> imagenes = new List<Image>();
+
+        public InterfazEdicion()
+        {
+
+            InitializeComponent();
+            openFileDialog1.ShowDialog(this);         
+            
+}
+
+        private void AbrirImagen()
+        {
+            try {
+
+            lector = new StreamReader(openFileDialog1.FileName);
+            
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.Size = pnPrevisualizacion.Size; // Tamaño de la imagen dentro del panel
+            pictureBox.SizeMode = PictureBoxSizeMode.StretchImage; // Escala la imagen para ajustarse al PictureBox
+            pictureBox.Image = Image.FromFile(openFileDialog1.FileName); // Carga la imagen
+            pnPrevisualizacion.Controls.Add(pictureBox);// Agrega el PictureBox al panel
+            
+            }
+            catch{
+            //error 
+            }
+        }
+
+
+
+        /*private void BitmapRecortar()
+        {
+            Rectangle rectOrig = new Rectangle(posXmin, posYmin, anchura, altura);
+            Bitmap source = new Bitmap(openFileDialog1.FileName);
+
+
+            Rectangle rectOrig = new Rectangle(posXmin, posYmin, anchura, altukra);
+
+            Bitmap CroppedImage = CropImage(source, rectOrig);hh
+
+        }
+        */
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+
+            InterfazPrincipal interfazPrincipal=new InterfazPrincipal();
+
+            // Mostrar el nuevo formulario
+            this.Visible = false;
+            interfazPrincipal.Show();
+
+
+        }
+
+
+        public void TPHoja(int tipoH)
+        {
+            Size tamañoHoja;
+            switch (tipoH)
+            {
+                case 1: // Carta
+                    tamañoHoja = new Size(2550 , 3300); // Tamaño en píxeles (ancho x alto)
+                    break;
+
+                case 2: // Oficio
+                    tamañoHoja = new Size(2550 , 4200); // Tamaño en píxeles (ancho x alto)
+                    break;
+
+                case 3: // A4
+                    tamañoHoja = new Size(2480 , 3508); // Tamaño en píxeles (ancho x alto)
+                    break;
+
+                // ------------------- 01/05/24 -----------------------------------------------
+                case 4:
+                    tamañoHoja = new Size(3508, 4961);
+                    break;
+
+                case 5:
+                    tamañoHoja = new Size(5100, 6600);
+                    break;
+
+
+                default:
+                    MessageBox.Show("Tipo de número no válido. Por favor, elija 1 para Carta o 2 para Oficio.");
+                    return;
+            }
+
+            // Crear el PictureBox para la previsualización de la hoja
+            Hoja = new PictureBox();
+            
+            Hoja.Left = 50;
+            Hoja.BackColor = Color.White;
+            Hoja.Top = 50;
+            Hoja.Size = tamañoHoja; // Tamaño del PictureBox igual al tamaño de la hoja
+            Hoja.SizeMode = PictureBoxSizeMode.Zoom; // Escalar la imagen para ajustarse al PictureBox
+
+            // Cargar la imagen de la hoja en el PictureBox
+            // (Asegúrate de reemplazar "HojaOriginal" con el nombre de tu imagen)
+            //Hoja.Image = Properties.Resources.HojaOriginal; // Cambiar "HojaOriginal" por el nombre de tu imagen
+            Hoja.Tag = tamañoHoja; // Almacenar el tamaño original de la imagen en el Tag del PictureBox
+            //Hoja.SizeChanged += Hoja_SizeChanged;
+            // Agregar el PictureBox al formulario
+            this.PanelPre.Controls.Add(Hoja);
+
+            // Agregar controles de zoom (por ejemplo, botones de zoom) al formulario
+            // (Agrega aquí los controles que permitirán al usuario hacer zoom en la imagen)
+        AbrirImagen(); // SELECCIONAR IMAGEN AL ABRIR LA VENTANA
+        }
+
+        // Método para hacer zoom en la imagen de la hoja
+        private void ZoomIn(PictureBox pictureBox)
+        {
+            Size tamañoOriginal = (Size)pictureBox.Tag;
+            Size tamañoActual = pictureBox.ClientSize;
+            int nuevoAncho = (int)(tamañoActual.Width * 1.1);
+            int nuevoAlto = (int)(tamañoActual.Height * 1.1);
+            nuevoAncho = Math.Min(nuevoAncho, tamañoOriginal.Width);
+            nuevoAlto = Math.Min(nuevoAlto, tamañoOriginal.Height);
+            pictureBox.ClientSize = new Size(nuevoAncho, nuevoAlto);
+        }
+
+        // Método para alejar la imagen de la hoja
+        private void ZoomOut(PictureBox pictureBox)
+        {
+            Size tamañoActual = pictureBox.ClientSize;
+            int nuevoAncho = (int)(tamañoActual.Width / 1.1);
+            int nuevoAlto = (int)(tamañoActual.Height / 1.1);
+            pictureBox.ClientSize = new Size(nuevoAncho, nuevoAlto);
+        }
+
+        // Ejemplo de manejo de eventos para los botones de zoom
+        private void btnZoomIn_Click(object sender, EventArgs e)
+        {
+            ZoomIn(Hoja);
+        }
+
+        private void btnZoomOut_Click(object sender, EventArgs e)
+        {
+            ZoomOut(Hoja);
+        }
+//
+        
+        private void NCopias_ValueChanged(object sender, EventArgs e)
+        {
+            //Hss
+            List<Image> imagenesDuplicadas = new List<Image>();
+            for (int i = 0; i < NCopias.Value; i++)
+            {
+                imagenesDuplicadas.AddRange(imagenes);
+            }
+
+            // Llamar a AgrImgHoj con las imágenes duplicadas
+            AgrImgHoj(imagenesDuplicadas);
+
+        }
+
+        private List<Image> ObtenerImagenesSeleccionadas()
+        {
+            List<Image> imagenes = new List<Image>();
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Archivos de imagen|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string filename in openFileDialog.FileNames)
+                {
+                    try
+                    {
+                        Image imagen = Image.FromFile(filename);
+                        imagenes.Add(imagen);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error al cargar la imagen: " + ex.Message);
+                    }
+                }
+            }
+
+            return imagenes;
+        }
+
+        private void btnAgregarImagenes_Click(object sender, EventArgs e)
+        {
+            // Este método será llamado cuando el usuario quiera agregar imágenes.
+            // Llamamos a ObtenerImagenesSeleccionadas para obtener las imágenes seleccionadas por el usuario.
+            imagenes = ObtenerImagenesSeleccionadas();
+
+            // Llamamos a AgrImgHoj con las imágenes obtenidas.
+            AgrImgHoj(imagenes);
+        }
+
+
+
+        private void AgrImgHoj(List<Image> imagenes)
+        {
+            Hoja.Controls.Clear();
+
+            // Dimensiones de la imagen
+            int imagenAncho = 100;
+            int imagenAlto = 100;
+
+            // Máximo número de imágenes por fila
+            int maxImagenesPorLinea = Hoja.Width / (imagenAncho + 20); // Se agrega un espacio de 20 píxeles entre cada imagen
+
+            // Posición inicial
+            int posX = 0;
+            int posY = 0;
+
+            foreach (Image imagen in imagenes)
+            {
+                // Crear un nuevo PictureBox para cada imagen
+                PictureBox pictureBox = new PictureBox();
+                pictureBox.Size = new Size(imagenAncho, imagenAlto);
+                pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox.Image = imagen;
+
+                // Establecer la posición del PictureBox
+                pictureBox.Location = new Point(posX, posY);
+
+                // Agregar el PictureBox a Hoja
+                Hoja.Controls.Add(pictureBox);
+
+                // Actualizar las coordenadas para la próxima imagen
+                posX += imagenAncho + 20; // 20 es el espacio entre cada imagen
+
+                // Si el número de imágenes en la fila actual alcanza el máximo, pasar a la siguiente fila
+                if (Hoja.Controls.Count % maxImagenesPorLinea == 0)
+                {
+                    posX = 0; // Reiniciar la posición X
+                    posY += imagenAlto + 20; // Moverse a la siguiente fila
+                }
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+      
+        }
+
+        private void pnPrevisualizacion_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void ExportarHojaComoJPG(string rutaArchivo)
+        {
+            Bitmap hojaBitmap = new Bitmap(Hoja.Width, Hoja.Height);
+
+            using (Graphics g = Graphics.FromImage(hojaBitmap))
+            {
+                // Dibujamos el fondo blanco
+                g.Clear(Color.White);
+
+                foreach (Control control in Hoja.Controls)
+                {
+                    if (control is PictureBox pictureBox)
+                    {
+                        // Obtenemos las coordenadas relativas del PictureBox en el contenedor
+                        Point pictureBoxLocation = pictureBox.Location;
+                        Point pictureBoxRelativeLocation = Hoja.PointToClient(pictureBoxLocation);
+
+                        // Dibujamos la imagen en las coordenadas relativas
+                        g.DrawImage(pictureBox.Image, pictureBoxRelativeLocation);
+                    }
+                }
+            }
+
+            hojaBitmap.Save(rutaArchivo, ImageFormat.Jpeg);
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            // Mostrar un cuadro de diálogo para que el usuario elija la ubicación y el nombre del archivo
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Archivos de imagen JPG|*.jpg";
+            saveFileDialog.Title = "Guardar hoja como JPG";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Llamar al método para exportar la hoja como JPG con la ruta de archivo seleccionada
+                ExportarHojaComoJPG(saveFileDialog.FileName);
+            }
+        }
+    } ////////////////////////////////
+
+}
+
+/* // Llamada a este método cuando quieras exportar la hoja como JPG, por ejemplo, desde un botón
+ private void btnExportarHoja_Click(object sender, EventArgs e)
+ {
+     // Mostrar un cuadro de diálogo para que el usuario elija la ubicación y el nombre del archivo
+     SaveFileDialog saveFileDialog = new SaveFileDialog();
+     saveFileDialog.Filter = "Archivos de imagen JPG|*.jpg";
+     saveFileDialog.Title = "Guardar hoja como JPG";
+     if (saveFileDialog.ShowDialog() == DialogResult.OK)
+     {
+         // Llamar al método para exportar la hoja como JPG con la ruta de archivo seleccionada
+         ExportarHojaComoJPG(saveFileDialog.FileName);
+     }
+ }
+
+ private void btnGuardar_Click(object sender, EventArgs e)
+ {
+     // Mostrar un cuadro de diálogo para que el usuario elija la ubicación y el nombre del archivo
+     SaveFileDialog saveFileDialog = new SaveFileDialog();
+     saveFileDialog.Filter = "Archivos de imagen JPG|*.jpg";
+     saveFileDialog.Title = "Guardar hoja como JPG";
+     if (saveFileDialog.ShowDialog() == DialogResult.OK)
+     {
+         // Llamar al método para exportar la hoja como JPG con la ruta de archivo seleccionada
+         ExportarHojaComoJPG(saveFileDialog.FileName);
+     }
+ }*/
